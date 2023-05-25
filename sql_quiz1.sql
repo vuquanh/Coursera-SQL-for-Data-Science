af1c8dd10e573cbdce02919226c3f2f3436b3b65
Anh Vu <88367747+vuquanh@users.noreply.github.com>
2023-05-25 00:16:46 -0700

    Update sql_quiz1.sql
    wrap code

--- sql_quiz1.sql
+++ sql_quiz1.sql
@@ -1,4 +1,5 @@
 -- All of the questions in this quiz refer to the open source Chinook Database. Please familiarize yourself with the ER diagram to familiarize yourself with the table and column names to write accurate queries and get the appropriate answers. https://ucde-rey.s3.amazonaws.com/DSV1015/ChinookDatabaseSchema.png
+
 --Q1 Find all the tracks that have a length of 5,000,000 milliseconds or more.
 SELECT trackID,
 milliseconds
